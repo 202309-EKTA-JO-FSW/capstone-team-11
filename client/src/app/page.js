3d27e15fe1654c03c78d0ec1cef729b671ef9002
Mohammad Al Issa <mohammed.alissa627@gmail.com>
2024-02-25 22:41:39 +0300

    Contact page add

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,12 @@
+import Contact from "./components/Contact";
 import Test from "./components/Test";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Test />
+        {/* <Test /> */}
+        <Contact></Contact>
       </div>
     </main>
   );
