0d44832bc8ff0805e01e68a006ee728bd99dee61
Mohammad Al Issa <mohammed.alissa627@gmail.com>
2024-03-04 21:03:07 +0300

    edit event form

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,4 +1,5 @@
 import Contact from "./components/Contact";
+import EventForm from "./components/EventForm";
 import Test from "./components/Test";
 
 export default function Home() {
@@ -6,7 +7,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         {/* <Test /> */}
-        <Contact></Contact>
+        {/* <Contact></Contact> */}
+        <EventForm />
       </div>
     </main>
   );
